12a413cff12ba100ccaa7d88ff936ef6ad2b9d42
yzy7720 <740343685@qq.com>
2022-01-06 23:58:35 +0800

    fix: editorconfig

--- src/index.tsx
+++ src/index.tsx
@@ -6,4 +6,5 @@ if ((module as any).hot) {
   (module as any).hot.accept();
 }
 
+// eslint-disable-next-line jsx-quotes
 ReactDOM.render(<App name="vortesnail" age={25} />, document.querySelector('#root'));
